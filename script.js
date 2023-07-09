54b7082e296f9df35dd012e7c7dc66444f9acdf5
Andrew <andrewnovosel90@gmail.com>
2023-07-09 17:13:20 -0400

    Added check and delete buttons and input for tasks

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const todoList = [
-    "Read chapter 7 of Harry Potter 4",
-    "Reason through JavaScript problems",
-    "Eat dinner",
-    "Do laundry",
-  ];
+  const todoList = [];
 
   const todoListElement = document.getElementById("todo-list");
+  const titleInput = document.getElementById("title");
+  const descriptionInput = document.getElementById("description");
+  const addButton = document.getElementById("add-btn");
 
-  todoList.forEach((item) => {
-    const li = document.createElement("li");
-    li.textContent = item;
-    todoListElement.append(li);
+  addButton.addEventListener("click", () => {
+    const title = titleInput.value;
+    const description = descriptionInput.value;
+
+    if (title && description) {
+      const newItem = {
+        title: title,
+        description: description,
+        completed: false,
+      };
+
+      todoList.push(newItem);
+
+      const li = document.createElement("li");
+      const textSpan = document.createElement("span");
+      textSpan.textContent = `${title}: ${description}`;
+      li.appendChild(textSpan);
+
+      const crossOffButton = document.createElement("button");
+      crossOffButton.innerHTML = '<i class="fas fa-check"></i>';
+      crossOffButton.classList.add("custom-cross-off-btn");
+      li.appendChild(crossOffButton);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+      deleteButton.classList.add("custom-delete-btn");
+      li.appendChild(deleteButton);
+
+      todoListElement.appendChild(li);
+
+      crossOffButton.addEventListener("click", () => {
+        textSpan.classList.add("completed");
+        newItem.completed = true;
+      });
+
+      deleteButton.addEventListener("click", () => {
+        const index = todoList.indexOf(newItem);
+        if (index > -1) {
+          todoList.splice(index, 1);
+        }
+        li.remove();
+      });
+
+      titleInput.value = "";
+      descriptionInput.value = "";
+    }
   });
 });
