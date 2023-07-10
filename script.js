477e28ea6974cf680b5e4318d5d05388e2756251
Andrew <andrewnovosel90@gmail.com>
2023-07-10 19:37:54 -0400

    Updated files

--- script.js
+++ script.js
@@ -1,85 +1,78 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const todoList = [];
+const todoList = [];
 
-  const todoListElement = document.getElementById("todo-list");
-  const titleInput = document.getElementById("title");
-  const descriptionInput = document.getElementById("description");
-  const addButton = document.getElementById("add-btn");
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const todoListElement = document.getElementById("todo-list");
 
-  addButton.addEventListener("click", () => {
-    const title = titleInput.value;
-    const description = descriptionInput.value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = input.value.trim();
+  if (todoText !== "") {
+    addTodoItem(todoText);
+    input.value = "";
+  }
+});
 
-    if (title && description) {
-      const newItem = {
-        title: title,
-        description: description,
-        completed: false,
-      };
+function addTodoItem(text) {
+  const todoItem = {
+    id: Date.now(),
+    text,
+    completed: false,
+  };
+  todoList.push(todoItem);
+  renderTodoItem(todoItem);
+}
 
-      todoList.push(newItem);
+function renderTodoItem(todoItem) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <input type="checkbox" class="checkbox">
+    <span>${todoItem.text}</span>
+    <button class="edit-button">Edit</button>
+    <button class="delete-button">Delete</button>
+  `;
 
-      const li = document.createElement("li");
-      const textSpan = document.createElement("span");
-      textSpan.textContent = `${title}: ${description}`;
-      li.appendChild(textSpan);
+  const checkbox = li.querySelector(".checkbox");
+  const span = li.querySelector("span");
+  const editButton = li.querySelector(".edit-button");
+  const deleteButton = li.querySelector(".delete-button");
 
-      const editButton = document.createElement("button");
-      editButton.innerHTML = '<i class="fas fa-edit"></i>';
-      editButton.classList.add("custom-edit-btn");
-      li.appendChild(editButton);
+  checkbox.addEventListener("change", () => {
+    todoItem.completed = checkbox.checked;
+    li.classList.toggle("completed", checkbox.checked);
+  });
 
-      const deleteButton = document.createElement("button");
-      deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
-      deleteButton.classList.add("custom-delete-btn");
-      li.appendChild(deleteButton);
+  editButton.addEventListener("click", () => {
+    span.contentEditable = true;
+    span.focus();
+    span.addEventListener("blur", handleEdit);
+    span.addEventListener("keydown", handleKeyPress);
+  });
 
-      todoListElement.appendChild(li);
-
-      deleteButton.addEventListener("click", () => {
-        const index = todoList.indexOf(newItem);
-        if (index > -1) {
-          todoList.splice(index, 1);
-        }
-        li.remove();
-      });
-
-      editButton.addEventListener("click", () => {
-        const newTitleInput = document.createElement("input");
-        newTitleInput.type = "text";
-        newTitleInput.value = newItem.title;
-
-        const newDescriptionInput = document.createElement("input");
-        newDescriptionInput.type = "text";
-        newDescriptionInput.value = newItem.description;
-
-        const saveButton = document.createElement("button");
-        saveButton.innerHTML = '<i class="fas fa-save"></i>';
-        saveButton.classList.add("custom-save-btn");
-        li.appendChild(saveButton);
-
-        li.replaceChild(newTitleInput, textSpan);
-        li.replaceChild(newDescriptionInput, editButton);
-        li.appendChild(saveButton);
-
-        saveButton.addEventListener("click", () => {
-          const newTitle = newTitleInput.value;
-          const newDescription = newDescriptionInput.value;
-
-          if (newTitle && newDescription) {
-            newItem.title = newTitle;
-            newItem.description = newDescription;
-            textSpan.textContent = `${newTitle}: ${newDescription}`;
-          }
-
-          li.replaceChild(textSpan, newTitleInput);
-          li.replaceChild(editButton, newDescriptionInput);
-          li.removeChild(saveButton);
-        });
-      });
-
-      titleInput.value = "";
-      descriptionInput.value = "";
+  deleteButton.addEventListener("click", () => {
+    const index = todoList.findIndex((item) => item.id === todoItem.id);
+    if (index !== -1) {
+      todoList.splice(index, 1);
+      li.remove();
     }
   });
-});
+
+  function handleEdit() {
+    const newText = span.textContent.trim();
+    if (newText !== "") {
+      todoItem.text = newText;
+    }
+    span.contentEditable = false;
+    span.removeEventListener("blur", handleEdit);
+    span.removeEventListener("keydown", handleKeyPress);
+  }
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      span.blur();
+    }
+  }
+
+  todoListElement.appendChild(li);
+}
